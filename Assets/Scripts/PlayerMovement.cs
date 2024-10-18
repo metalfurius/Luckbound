e1f34abed60ce0b9175cfe0c62e3c6dd0bac4bf8
metalfurius <hackero1932@gmail.com>
2024-10-18 17:28:58 +0200

    fix: ground check now has 2 raycast

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(PlayerInput))]
@@ -7,8 +8,10 @@ public class PlayerMovement : MonoBehaviour
     [Header("Player Movement Settings")]
     [SerializeField] private float moveSpeed = 5f;
     
+    [FormerlySerializedAs("groundCheck")]
     [Header("Ground Check Settings")]
-    [SerializeField] private Transform groundCheck;  
+    [SerializeField] private Transform groundCheck1;  
+    [SerializeField] private Transform groundCheck2;  
     [SerializeField] private float groundCheckRadius = 0.1f;
     [SerializeField] private LayerMask groundLayer;
     
@@ -31,16 +34,28 @@ public class PlayerMovement : MonoBehaviour
     }
     private void Update()
     {
+        HandleMovement();
+        HandleJump();
+    }
+
+    private void HandleMovement()
+    {
         var _moveDirection = new Vector2(playerInput.MovementInput.x, 0);
         rb.linearVelocity = new Vector2(_moveDirection.x * moveSpeed, rb.linearVelocity.y);
+    }
+
+    private void HandleJump()
+    {
         isGrounded = IsGrounded();
 
-        if (isGrounded)
-            jumpCount = 0;
         
         if (playerInput.JumpInput && (isGrounded || jumpCount < maxJumps))
             Jump();
+        
+        if (isGrounded)
+            jumpCount = 0;
     }
+
     private void Jump()
     {
         rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f); 
@@ -49,7 +64,9 @@ public class PlayerMovement : MonoBehaviour
     }
     private bool IsGrounded()
     {
-        var _hit = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckRadius, groundLayer);
-        return _hit.collider;
+        var _hit1 = Physics2D.Raycast(groundCheck1.position, Vector2.down, groundCheckRadius, groundLayer);
+        var _hit2 = Physics2D.Raycast(groundCheck2.position, Vector2.down, groundCheckRadius, groundLayer);
+        
+        return _hit1.collider || _hit2.collider  ;
     }
 }
