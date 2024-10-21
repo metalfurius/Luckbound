4c6d524f5acd1561152fd74e8701940f10654dda
metalfurius <hackero1932@gmail.com>
2024-10-21 23:32:22 +0200

    feat: added basic enemy states

--- Assets/Scripts/Interactions/Enemies/Enemy.cs
+++ Assets/Scripts/Interactions/Enemies/Enemy.cs
@@ -1,8 +1,101 @@
+using System;
 using UnityEngine;
 
 public class Enemy : Damageable
 {
     public int resistance = 5;
+    public EnemyState currentState = EnemyState.Idle;
+    public BoxCollider2D areaOfInfluence;
+
+    private void Start()
+    {
+        areaOfInfluence = GetComponent<BoxCollider2D>(); 
+        if (areaOfInfluence)
+            areaOfInfluence.isTrigger = true; 
+        else
+            Debug.LogError("Area of influence collider not found!");
+    }
+
+    public virtual void Update()
+    {
+        switch (currentState)
+        {
+            case EnemyState.Idle:
+                HandleIdleState();
+                break;
+            case EnemyState.Patrol:
+                HandlePatrolState();
+                break;
+            case EnemyState.Chase:
+                HandleChaseState();
+                break;
+            case EnemyState.Attack:
+                HandleAttackState();
+                break;
+            case EnemyState.Flee:
+                HandleFleeState();
+                break;
+            case EnemyState.Die:
+                HandleDieState();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    public virtual void HandleAttackState()
+    {
+    }
+
+    public virtual void HandleIdleState() 
+    {
+    }
+
+    public virtual void HandlePatrolState() 
+    {
+    }
+
+    public virtual void HandleChaseState() 
+    {
+    }
+    public virtual void HandleFleeState() 
+    {
+    }
+    public virtual void HandleDieState() 
+    {
+    }
+
+    protected virtual void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            OnPlayerEnterArea(); 
+        }
+    }
+
+    protected virtual void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            OnPlayerExitArea();
+        }
+    }
+
+    public virtual void OnPlayerEnterArea()
+    {
+        if (currentState is EnemyState.Idle or EnemyState.Patrol)
+        {
+            currentState = EnemyState.Chase;
+        }
+    }
+
+    public virtual void OnPlayerExitArea()
+    {
+        if (currentState == EnemyState.Chase)
+        {
+            currentState = EnemyState.Idle; // Or back to Patrol
+        }
+    }
 
     public override void TakeDamage(int amount)
     {
@@ -12,6 +105,7 @@ public class Enemy : Damageable
 
     protected override void Die()
     {
+        currentState = EnemyState.Die;
         Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/Interactions/Enemies/EnemyState.cs
@@ -0,0 +1,9 @@
+public enum EnemyState
+{
+    Idle,
+    Patrol,
+    Chase,
+    Attack,
+    Flee,
+    Die 
+}
