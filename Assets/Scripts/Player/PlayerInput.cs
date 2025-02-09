4c4883eb4e56cc3b2eb3f91290fe2cdf69469ad8
metalfurius <hackero1932@gmail.com>
2025-02-09 13:24:49 +0100

    No compression

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -5,15 +5,17 @@ public class PlayerInput : MonoBehaviour
     public Vector2 MovementInput { get; private set; }
     public bool JumpInputDown { get; private set; }
     public bool JumpInputUp { get; private set; }
+    public bool AttackInput { get; private set; }
 
     private void Update()
     {
-        var _moveX = Input.GetAxis("Horizontal");
-        var _moveY = Input.GetAxis("Vertical");
+        var moveX = Input.GetAxis($"Horizontal");
+        var moveY = Input.GetAxis($"Vertical");
 
-        MovementInput = new Vector2(_moveX, _moveY);
-
-        JumpInputDown = Input.GetButtonDown("Jump");
-        JumpInputUp = Input.GetButtonUp("Jump");
+        MovementInput = new Vector2(moveX, moveY);
+        
+        AttackInput = Input.GetButtonDown($"Fire1");
+        JumpInputDown = Input.GetButtonDown($"Jump");
+        JumpInputUp = Input.GetButtonUp($"Jump");
     }
 }
